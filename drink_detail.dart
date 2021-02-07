af5524c1b0c547916b86a01617a6e41f7772c1dd
Rajan26 <52351926+Rajan26@users.noreply.github.com>
2021-02-07 20:15:49 +0530

    Add files via upload

---
+++ drink_detail.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/main.dart';
+
+class DrinkDetail extends StatelessWidget {
+  final drink;
+
+  const DrinkDetail({Key key, @required this.drink}) : super(key: key);
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: myColor,
+      appBar: AppBar(
+        title: Text(drink["strDrink"]),
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+              Hero(
+                tag: drink["idDrink"],
+                child: CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: NetworkImage(drink["strDrinkThumb"],
+                  ),
+                  ),
+              ),
+              SizedBox(
+                height: 30.0,
+
+              ),
+              Text(
+              "ID: ${drink["idDrink"]}",
+              style: TextStyle(
+                
+                color: Colors.white
+              ),
+            ),
+            SizedBox(
+                height: 30.0,
+
+              ),
+            Text(
+              "${drink["strDrink"]}",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/homepage.dart';
+
+const myColor = Colors.red;
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cocktail App',
+      theme: ThemeData(
+        
+          primarySwatch: myColor,
+          
+       
+       
+      
+      
+      ),
+      home: HomePage(),
+    );
+    
+  }
+}
+
